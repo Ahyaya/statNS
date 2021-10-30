5e9a02336d95b2b1fee5cc3891132d75d7448c03
chan <c.houyuan@mail.scut.edu.cn>
2021-10-31 03:38:36 +0800

    update test_EoSlib.c need to improve M2Rhoc_fm() efficiency

--- test_EoSlib.c
+++ test_EoSlib.c
@@ -7,47 +7,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #include "src/statNS.h"
 
 int main(){
+
+    char EoS_lib_dir[64]="EoS_lib/";
+
+    /*判断路径是否以斜杠结尾，没有的话自动加上*/
+    EoS_lib_dir[strlen(EoS_lib_dir)]=(EoS_lib_dir[strlen(EoS_lib_dir)-1]==0x2F)?0x00:0x2F;
+
     int EoSlistLen=0,pf;
+    DIR *dirPt;
+    struct dirent *fileBlock;
+
+    if((dirPt=opendir(EoS_lib_dir))==NULL){
+        fprintf(stderr,"unable to access directory\n");
+        return -1;
+    }
     
-    Path_t EoSlist[1000];/*用来存放EoS的文件路径，这里能存1000个*/
+    Path_t EoSlist[8];/*用来存放EoS的文件路径，这里能存8个*/
+    CompactStar_t Results[8];/*用来存放计算结果*/
     
     /*用你喜欢的办法将EoS文件的路径写进EoSlist[].FilePath上*/
-    system("ls EoS_lib/*.txt > tmpEoS.list");
-    FILE *fp=fopen("tmpEoS.list","r");
-    while(fgets(EoSlist[EoSlistLen].FilePath,128,fp)){
-        EoSlist[EoSlistLen].FilePath[strlen(EoSlist[EoSlistLen].FilePath)-1]=0x00;
-        EoSlistLen++;
+    while(fileBlock=readdir(dirPt)){
+        if(EoSlistLen<8){
+            /*判断是否为current dir和parent dir标识符*/
+            if(fileBlock->d_name[0]==0x2E){
+                continue;
+            }
+            sprintf(EoSlist[EoSlistLen].FilePath,"%s%s",EoS_lib_dir,fileBlock->d_name);
+            EoSlistLen++;
+        }else{
+            m2fmode_mt(Results,1.4,EoSlist,EoSlistLen,8);
+            for(pf=0;pf<EoSlistLen;pf++){
+                fprintf(stderr,"%s Rho=%.3e M=%.3lf freq=%lf dptime=%.3lf\n",EoSlist[pf].FilePath,Results[pf].Rho,Results[pf].M,Results[pf].freq,Results[pf].dampTime);
+            }
+            EoSlistLen=0;
+        }
     }
-    fclose(fp);
+    closedir(dirPt);
 
-/*初始化一个数组来存放计算结果，它未必要跟EoS的总数量一样长，因为你可以分多次进行计算*/
-    CompactStar_t Results[EoSlistLen];
-
-/*这里指定用6个线程进行计算，结果存放到Results[]上，目标质量为1.654321倍太阳质量，EoS文件名列表入口，总共要计算的EoS数量*/
-    m2fmode_mt(Results,1.654321,EoSlist,EoSlistLen,6);
+    /*check if all EoS have been computed*/
+    if(EoSlistLen){
+        m2fmode_mt(Results,1.4,EoSlist,EoSlistLen,8);
+        for(pf=0;pf<EoSlistLen;pf++){
+            fprintf(stderr,"%s Rho=%.3e M=%.3lf freq=%lf dptime=%.3lf\n",EoSlist[pf].FilePath,Results[pf].Rho,Results[pf].M,Results[pf].freq,Results[pf].dampTime);
+        }
+        EoSlistLen=0;
+    }
     
-/*为了能操控中间结果，我推荐每次不要算太多，这样数组Results[]可以短一些，
-* 而且计算虽然是多线程，但是在没有全部完成之前你是无法查看中间任何结果的，
-* 为了防止出现计算崩溃导致结果全部丢失的问题，我建议手动对EoSlist进行分割，
-* 比如说，10000个物态方程，我可以先将它们的路径全部保存至EoSlist[]，此时EoSlistLen=10000，
-* 我们可以通过调用50次 m2fmode_fm() 函数来达到计算目的（这样每次要计算200个）：
-* CompactStar_t Results[200];
-* int pf;
-* for(pf=0;pf<50;pf++){
-*     m2fmode_fm(10,Results,1.44,EoSlist+200*pf,200);
-*     ...
-* }
-* 
-*/
-
-/*一种直接打印结果的方法，当然你可以对这个结果做其他处理*/
-    for(pf=0;pf<EoSlistLen;pf++){
-        fprintf(stderr,"%s Rho=%e M=%lf freq=%lf damptime=%lf\n",EoSlist[pf].FilePath,Results[pf].Rho,Results[pf].M,Results[pf].freq,Results[pf].dampTime);
-    }
-	
     return 0;
 }
